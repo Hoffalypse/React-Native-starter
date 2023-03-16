494ab9a1fe0cc5d55a2dacb7e0662a819e9d82b5
Bryan W <bhoff1980@gmail.com>
2023-03-16 02:11:09 -0600

    initial file structure setup

---
+++ src/screens/CompnentsScreen.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const CompnentsScreen = () => {
+  return (
+    
+      <Text style={styles.textStyle}>This is the CompnentsScreen</Text>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
+    }
+})
+
+export default CompnentsScreen
