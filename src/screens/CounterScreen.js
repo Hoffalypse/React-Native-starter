b59971ae437151727669abc1784df604fcdd33f0
Bryan W <bhoff1980@gmail.com>
2023-03-17 00:49:04 -0600

    refactor counter page from useState to useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,20 +1,43 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
-import React, {useState} from 'react'
+import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useReducer } from "react";
+
+const reducer = (state, action) => {
+  
+  switch (action.type) {
+    case "goUp":
+      return {...state, counter: state.counter + action.payload};
+
+    case "goDown":
+      return {...state, counter: state.counter - action.payload};
+
+    default:
+      return state;
+  }
+};
 
 const CounterScreen = () => {
-let [counter, setCounter] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+
+
 
   return (
     <View>
-        <Button title="Increase" onPress={() => {
-            setCounter(counter+1)
-        }}/>
-        <Button title='Decrease' onPress={() => {
-            setCounter(counter-1)
-        }}/>
-      <Text>Current Count: {counter}</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          dispatch({ type: "goUp", payload: 1 });
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          dispatch({ type: "goDown", payload: 1 });
+        }}
+      />
+      <Text>Current Count: {state.counter}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CounterScreen
+export default CounterScreen;
