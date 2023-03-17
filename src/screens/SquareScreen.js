3258f1e694a66bade850ff2bd8fef5eb115b4ba8
Bryan W <bhoff1980@gmail.com>
2023-03-17 00:24:58 -0600

    use reducer setup using common conventions

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,49 +2,52 @@ import { View, Text } from "react-native";
 import React, { useReducer } from "react";
 import ColorCounter from "../components/ColorCounter";
 
-const increment = 10
+const increment = 10;
 const reducer = (state, action) => {
-    switch (action.colorToChange) {
-        case 'red':
-            return{...state, red: state.red + action.amount}
-            
-        case 'green':
-            return{...state, green: state.green + action.amount}
-            
-            case 'blue':
-                return{...state, blue: state.blue + action.amount}
-        default:
-            return state
-    }
-    
-}
+  switch (action.type) {
+    case "red":
+      return state.red + action.payload > 255 || state.red + action.payload < 0
+        ? state
+        : { ...state, red: state.red + action.payload };
+
+    case "green":
+      return state.green + action.payload > 255 ||
+        state.green + action.payload < 0
+        ? state
+        : { ...state, green: state.green + action.payload };
+
+    case "blue":
+      return state.blue + action.payload > 255 ||
+        state.blue + action.payload < 0
+        ? state
+        : { ...state, blue: state.blue + action.payload };
+    default:
+      return state;
+  }
+};
 
 const SquareScreen = () => {
-//   const [red, setRed] = useState(0);
-//   const [green, setGreen] = useState(0);
-//   const [blue, setBlue] = useState(0);
+  
 
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
 
-const [state, dispatch] = useReducer(reducer, {red:0, green:0, blue:0})
-
-
-
+  const { red, green, blue } = state;
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() => dispatch({ type: "red", payload: increment })}
+        onDecrease={() => dispatch({ type: "red", payload: -1 * increment })}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() => dispatch({ type: "green", payload: increment })}
+        onDecrease={() => dispatch({ type: "green", payload: -1 * increment })}
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() => dispatch({ type: "blue", payload: increment })}
+        onDecrease={() => dispatch({ type: "blue", payload: -1 * increment })}
         color="Blue"
       />
       <View
@@ -54,7 +57,10 @@ const [state, dispatch] = useReducer(reducer, {red:0, green:0, blue:0})
           backgroundColor: `rgb(${red}, ${green}, ${blue})`,
         }}
       />
-      <Text> rgb({red}, {green}, {blue}) </Text>
+      <Text>
+        {" "}
+        rgb({red}, {green}, {blue}){" "}
+      </Text>
     </View>
   );
 };
